052bb861331ae7cb01c82cfa53a0798d037a08e6
Mauz Khan <115772334+mauzkhan13@users.noreply.github.com>
2024-06-15 22:36:51 +0500

    Update stubhub.py

--- stubhub/stubhub.py
+++ stubhub/stubhub.py
@@ -26,7 +26,7 @@ def get_browser():
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-logging')
     options.add_argument('--log-level=3')
-    # options.add_argument('--headless')
+    options.add_argument('--headless')
     options.add_argument('--window-size=1920,1080')
     options.binary_location = '/usr/bin/google-chrome' 
   
