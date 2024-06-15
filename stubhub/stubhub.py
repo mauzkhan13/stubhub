cd9b076f791f80bdbaba106266a3c482d6a0ee24
Mauz Khan <115772334+mauzkhan13@users.noreply.github.com>
2024-06-15 13:02:20 +0500

    Update stubhub.py

--- stubhub/stubhub.py
+++ stubhub/stubhub.py
@@ -39,6 +39,7 @@ def get_browser():
 def event_urls(browser):
     next_page = WebDriverWait(browser, 3).until(
                 EC.element_to_be_clickable((By.XPATH, '(//*[contains(text(),"See more events")])[2]'))
+    )  # This closing parenthesis was missing
     next_page.click()
         
     events_links = []
