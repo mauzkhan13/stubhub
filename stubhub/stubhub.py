1c00cc6735c7e0077dbf26de5d81546fce0e7cbc
Mauz Khan <115772334+mauzkhan13@users.noreply.github.com>
2024-06-15 23:47:24 +0500

    Update stubhub.py

--- stubhub/stubhub.py
+++ stubhub/stubhub.py
@@ -74,10 +74,10 @@ def event_urls(browser):
     #     action = ActionChains(browser)
     #     action.move_to_element(t).perform()
     #     t.click()
-    see_more_button = browser.find_element(By.XPATH, '//footer//button[contains(text(), "See more events")]')
+    see_more_button = browser.find_element(By.XPATH, '//div//button[contains(text(), "See more events")]')
 
     # Wait until the button is clickable
-    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, '//footer//button[contains(text(), "See more events")]')))
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, '//div//button[contains(text(), "See more events")]')))
    
     # Click the button
     see_more_button.click()
