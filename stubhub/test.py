1460451969e59ce9bef74bcbc1241d0b1b7cdbdd
Mauz Khan <115772334+mauzkhan13@users.noreply.github.com>
2024-06-16 08:46:47 +0500

    Update test.py

--- stubhub/test.py
+++ stubhub/test.py
@@ -7,7 +7,7 @@ def run(playwright):
 
     url = "https://www.stubhub.ie/euro-2024-tickets/grouping/1507012/?wcpb=4"
     page.goto(url)
-
+    f("The Browser is opened")
     page.wait_for_load_state()
     time.sleep(1)
 
@@ -25,6 +25,7 @@ def run(playwright):
             next_page = page.locator('(//*[contains(text(),"See more events")])[2]')
             if next_page.is_visible():
                 next_page.click()
+                f("The next page is clicking")
                 time.sleep(1)
             else:
                 break
