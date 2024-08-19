200953ab593f2c40c73670345404e6ad7eac424d
Mauz Khan <115772334+mauzkhan13@users.noreply.github.com>
2024-08-19 21:56:08 +0500

    Update test.py

--- stubhub/test.py
+++ stubhub/test.py
@@ -259,7 +259,7 @@ def process_url(index, url):
 
 def main():
     urls = event_urls()
-    with concurrent.futures.ThreadPoolExecutor(max_workers=15) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
         futures = [executor.submit(process_url, index, url) for index, url in enumerate(urls)]
         concurrent.futures.wait(futures)
 
